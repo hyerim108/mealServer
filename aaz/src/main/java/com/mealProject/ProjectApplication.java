72faeb76141087cf962bd3b87482ca6bc267dc0d
DESKTOP-IRSLAJA\user <batlove108@naver.com>
2021-12-10 14:48:50 +0900

    springboot

--- aaz/src/main/java/com/mealProject/ProjectApplication.java
+++ aaz/src/main/java/com/mealProject/ProjectApplication.java
@@ -2,12 +2,28 @@ package com.mealProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class ProjectApplication {
+//@SpringBootApplication
+//public class ProjectApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ProjectApplication.class, args);
+//	}
+//
+//}
+public class ProjectApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+public static void main(String[] args) {
+	SpringApplication applicatino = new SpringApplication(ProjectApplication.class);
+}
+
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// TODO Auto-generated method stub
+	return builder.sources(ProjectApplication.class);
+}
 
 }
+
